0c2121da2fe2781efb1c928394976ed948ba1847
scamdivine <72702048+scamdivine@users.noreply.github.com>
2021-09-09 16:45:55 +0200

    validation for charger id  (#24)
    * adds validation to charger id and creates a button with corresponding color
    
    * cleand up code and fixed requested changes

--- FlexiCharge/FlexiCharge/Views/IdentifyCharger/IdentifyChargerView.swift
+++ FlexiCharge/FlexiCharge/Views/IdentifyCharger/IdentifyChargerView.swift
@@ -13,9 +13,15 @@ struct IdentifyChargerView: View {
     @State private var username: String = ""
     @State private var isEditing: Bool = false
     @State private var chargerIdInput: String = ""
+    @State private var buttonText: String = ""
+    @State private var buttonColor: Color = .white
+    @State private var isButtonDisabled: Bool = true
+    @State private var isButtonVisible: Double = 0
+    @State private var buttonTextColor: Color = .clear
     @State var value: CGFloat = 0
     @State var keyboardHeight: CGFloat = 0
-    
+
+
     var body: some View {
         ZStack (alignment: .top){
             RoundedRectangle(cornerRadius: 5)
@@ -23,7 +29,6 @@ struct IdentifyChargerView: View {
                 .frame(minHeight: 0, maxHeight: .infinity)
             VStack {
                 Image("menu-arrow").rotationEffect(.degrees(180))
-                
                 Text("Chargers Near Me")
                     .foregroundColor(.white)
                     .padding(.bottom, 40)
@@ -60,19 +65,76 @@ struct IdentifyChargerView: View {
                             validateChargerId()
                         })
                 }
-                
-                Text("Enter the Code Written on the Charger")
-                    .foregroundColor(.white)
+                ZStack {
+                    Text("Enter the Code Written on the Charger")
+                        .foregroundColor(.white)
+                        .opacity(isButtonVisible == 1 ? 0 : 1)
+                    Button(action: {
+                        startCharging()
+                    }){
+                        Text(buttonText)
+                            .font(Font.system(size: 20,weight: .bold, design: .default))
+                            .foregroundColor(buttonTextColor)
+                    }.frame(width: 300, height: 53, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                    .background(buttonColor)
+                    .cornerRadius(5)
+                    .disabled(isButtonDisabled)
+                    .opacity(isButtonVisible)
+                }
             }.padding()
         }
     }
     // Makes sure the entered charger id is not too long or is not all integers
     func validateChargerId() {
         let limit: Int = 6
+        if isButtonVisible == 1 && chargerIdInput.count < limit {
+            isButtonDisabled = true
+            isButtonVisible = 0
+        }
         if chargerIdInput.count > limit || Int(chargerIdInput) == nil && chargerIdInput.count > 0 {
             chargerIdInput.removeLast()
+        } else if chargerIdInput.count == limit {
+            let status = getChargerStatus(chargerId: Int(chargerIdInput)!)
+            drawChargingButton(status: status)
         }
     }
+    
+    func drawChargingButton(status: Int) {
+        let notIdentified: Int = 0
+        let success: Int = 1
+        let occupied: Int = 2
+        let outOfOrder: Int = 3
+        if status == notIdentified {
+            buttonText = "Charger Not Identified"
+            buttonColor = Color(red: 0.90, green: 0.90, blue: 0.90)
+            isButtonDisabled = true
+            isButtonVisible = 1
+            buttonTextColor = Color(red: 0.30, green: 0.30, blue: 0.30)
+        } else if status == success {
+            buttonText = "Begin Charging"
+            buttonColor = Color(red: 0.47, green: 0.74, blue: 0.46)
+            isButtonDisabled = false
+            isButtonVisible = 1
+            buttonTextColor = Color(red: 1.00, green: 1.00, blue: 1.00)
+        } else if status == occupied {
+            buttonText = "Charger Occupied"
+            buttonColor = Color(red: 0.94, green: 0.38, blue: 0.28)
+            isButtonDisabled = true
+            isButtonVisible = 1
+            buttonTextColor = Color(red: 1.00, green: 1.00, blue: 1.00)
+        } else if status == outOfOrder {
+            buttonText = "Charger Out of Order"
+            buttonColor = Color(red: 0.90, green: 0.90, blue: 0.90)
+            isButtonDisabled = true
+            isButtonVisible = 1
+            buttonTextColor = Color(red: 0.30, green: 0.30, blue: 0.30)
+        }
+    }
+}
+
+func startCharging(){
+    //Add functionality to startChargingButton
+    //Send all selected options to API
 }
 
 struct IdentifyChargerView_Previews: PreviewProvider {
@@ -86,4 +148,3 @@ extension String {
         return String(self[index(startIndex, offsetBy: i)])
     }
 }
-
---
+++ FlexiCharge/FlexiCharge/Views/IdentifyCharger/IdentifyChargerViewModel.swift
@@ -0,0 +1,18 @@
+//
+//  IdentifyChargerViewModel.swift
+//  FlexiCharge
+//
+//  Created by Daniel Göthe on 2021-09-09.
+//
+
+import Foundation
+let testChargers: [Int : Int] = [111111: 1, 123456: 2, 133777: 3]
+
+
+func getChargerStatus(chargerId: Int) -> Int {
+    let notIdentified: Int = 0
+    if testChargers[chargerId] == nil {
+            return notIdentified
+    }
+    return testChargers[chargerId]!
+}
