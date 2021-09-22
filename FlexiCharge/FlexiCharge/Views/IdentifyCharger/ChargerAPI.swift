da5613f547493076be6a6f4b12a972a9958402fd
Filip Flodén <48211957+filipfloden@users.noreply.github.com>
2021-09-22 14:14:42 +0200

    Created a ChargerAPI that can fetch all chargers

---
+++ FlexiCharge/FlexiCharge/Views/IdentifyCharger/ChargerAPI.swift
@@ -0,0 +1,45 @@
+//
+//  ChargerAPI.swift
+//  FlexiCharge
+//
+//  Created by Filip Flodén on 2021-09-22.
+//
+
+import Foundation
+import Combine
+import SwiftUI
+
+struct ChargerTest: Decodable {
+    var chargerID: Int
+    var location: [CGFloat]
+    var chargePointID: Int
+    var serialNumber: String
+    var status: Int
+}
+
+struct ChargerListTest: Decodable {
+    var results:  [ChargerTest]
+}
+
+class ChargerAPI {
+    var didChange = PassthroughSubject<ChargerAPI, Never>()
+    var chargers = [ChargerTest](){
+        didSet {
+            didChange.send(self)
+        }
+    }
+    
+    init() {
+        // Fetches all chargers
+        guard let url = URL(string: "http://54.220.194.65:8080/chargers") else { return }
+        URLSession.shared.dataTask(with: url) { (data, _, _) in
+            guard let data = data else { return }
+            let decodedData = try! JSONDecoder().decode([ChargerTest].self, from: data)
+            
+            DispatchQueue.main.async {
+                self.chargers = decodedData
+            }
+            print(decodedData)
+        }.resume()
+    }
+}
--- FlexiCharge/FlexiCharge/Views/SetupInvoice/SSNApi.swift
+++
@@ -1,9 +0,0 @@
-//
-//  SSNApi.swift
-//  FlexiCharge
-//
-//  Created by Lucas Strand on 2021-09-20.
-//
-
-import Foundation
-
