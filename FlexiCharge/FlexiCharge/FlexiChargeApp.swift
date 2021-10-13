5585e134527ba2a7ca172a0ed2cf2c7138114376
Filip Flodén <48211957+filipfloden@users.noreply.github.com>
2021-10-13 13:38:40 +0200

    Removed unused code

--- FlexiCharge/FlexiCharge/FlexiChargeApp.swift
+++ FlexiCharge/FlexiCharge/FlexiChargeApp.swift
@@ -18,19 +18,3 @@ struct FlexiChargeApp: App {
         }
     }
 }
-
-class appDelegate: NSObject, UIApplicationDelegate {
-    func application(_ application: UIApplication,
-                     open url: URL,
-                     options: [UIApplication.OpenURLOptionsKey : Any] = [:] ) -> Bool {
-        let sendingAppID = options[.sourceApplication]
-        print("source application = \(sendingAppID ?? "Unknown")")
-        guard let components = NSURLComponents(url: url, resolvingAgainstBaseURL: true),
-             let albumPath = components.path,
-            let params = components.queryItems else {
-                print("Invalid URL or album path missing")
-                return false
-        }
-        return true
-    }
-}
